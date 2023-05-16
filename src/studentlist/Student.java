3132620f6a107a81742bd0f3618e9d90a6c69fb0
Robert Orazu <orazurobert@yahoo.com>
2023-05-16 14:59:32 +0100

    First commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author robert
+ */
+public class Student {
+    private String sname;
+    private String sID;
+
+    public Student(String sID, String sname) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+    
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author robert
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Scanner input = new Scanner(System.in);
+    Student[] studentList = new Student[3];
+    studentList[0] = new Student("s1", "Robert");
+    studentList[1] = new Student("s2", "Rashed");
+    studentList[2] = new Student("s3", "Het");
+    
+        for(int i = 0; i < studentList.length; i++) {
+            System.out.println(studentList[i].getSname());
+        }
+    }
+    
+}
